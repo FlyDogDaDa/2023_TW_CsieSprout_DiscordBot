bfb4c51d039903038466bfb6561198eef1599fde
飛天柴犬 <54river0522@gmail.com>
2023-05-14 17:09:37 +0800

    更換TOKNE

--- bot.py
+++ bot.py
@@ -1,37 +1,40 @@
 # Import libraries
 import discord
 from discord.ext import commands
-import os  
+import os
 from dotenv import load_dotenv
 
-# Set bots 
+# Set bots
 load_dotenv()
-bot = commands.Bot(command_prefix ="$", intents = discord.Intents.all()) 
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
 
 @bot.event
 async def on_ready():
-    for FileName in os.listdir('./cmds'):
-        if FileName.endswith('.py'):
-            bot.load_extension(f'cmds.{FileName[:-3]}')
-    
+    for FileName in os.listdir("./cmds"):
+        if FileName.endswith(".py"):
+            bot.load_extension(f"cmds.{FileName[:-3]}")
+
     print(">>Bot is Online<<")
+
 
 @bot.command()
 async def load(ctx, extension):
-    bot.load_extension(f'cmds.{extension}')
-    await ctx.send(f'Loaded')
+    bot.load_extension(f"cmds.{extension}")
+    await ctx.send(f"Loaded")
+
 
 @bot.command()
 async def reload(ctx, extension):
-    bot.reload_extension(f'cmds.{extension}')
-    await ctx.send(f'Reloaded')
+    bot.reload_extension(f"cmds.{extension}")
+    await ctx.send(f"Reloaded")
+
 
 @bot.command()
 async def unload(ctx, extension):
-    bot.unload_extension(f'cmds.{extension}')
-    await ctx.send(f'Unloaded')
+    bot.unload_extension(f"cmds.{extension}")
+    await ctx.send(f"Unloaded")
 
 
-if  __name__ == "__main__":
-    bot.run(os.getenv('TOKEN'))
+if __name__ == "__main__":
+    bot.run(os.getenv("TOKEN"))
