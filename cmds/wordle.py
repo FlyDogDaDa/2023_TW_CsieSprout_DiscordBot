ab75fbb4c8b32fca48dfcae610b6982c21a5b76a
飛天柴犬 <54river0522@gmail.com>
2023-06-18 15:35:43 +0800

    合併Yummy Berry的程式
    Yummy Berry在外部開發完成此遊戲程式，現在要將此程式合併至主要枝幹中。

--- cmds/wordle.py
+++ cmds/wordle.py
@@ -1,48 +1,116 @@
 import discord
 from discord.ext import commands
-import json
 from core import Cog_Extension
-import urllib
-import random
+from bs4 import BeautifulSoup
+import json, requests, random
+
+
+class Player:
+    def __init__(self, answer):
+        self.answer_times = 0
+        self.right_words = 0
+        # self.player = ""
+        self.answer = answer
+        # self.in_game = False
+        self.alpha = [0 for i in range(26)]
+        self.in_game = False
 
 
 class Wordle(Cog_Extension):
-    # Initialization
     def __init__(self, bot):
-        pass
+        super().__init__(bot)
+        with open("data.json", "r", encoding="utf8") as data:
+            jdata = json.load(data)
+        r = requests.get(
+            "https://gist.githubusercontent.com/cfreshman/d97dbe7004522f7bc52ed2a6e22e2c04/raw/633058e11743065ad2822e1d2e6505682a01a9e6/wordle-nyt-words-14855.txt"
+        )
+        soup = BeautifulSoup(r.text, "html.parser")
 
-        """
-        TODO 
-        要在init function 中載入單字庫
+        self.players = {}
+        # self.player = ""
+        # self.answer = ""
+        # self.in_game = False
 
-        Hint:
-        1. 好像有import urllib
-        2. data.json中有貼上url了
-        """
+        self.words = str(soup).split("\n")
+        # self.answer_times = 0
+        # self.alpha = [0 for i in range(26)]
+        # self.right_words = 0
 
-    @commands.command()
-    async def Play(self, ctx):
-        pass
+    @commands.command(pass_context=True)
+    async def ask(self, ctx, response):
+        response = response.lower()
+        # print(response)
+        # global alpha, answer_times, right_words, player, answer, in_game
+        player = self.players.get(ctx.author, 0)
+        if player == 0 or not player.in_game:
+            await ctx.channel.send("The game doesn't start!")
+            return
+        # if ctx.author != self.player:
+        #     await ctx.channel.send("You aren't player!")
+        #     return
+        if len(response) != 5:
+            await ctx.channel.send(
+                "The word must consist of five letters, please guess again."
+            )
+            return
+        if response not in self.words:
+            await ctx.channel.send(f"Not a word, please guess again.")
+            return
 
-        """
-        TODO 
-        要在爬好的單字庫中, 隨機挑選一個單字做為預設的答案
-        """
+        for char in player.answer:
+            if ord(char) >= ord("a") and ord(char) <= ord("z"):
+                player.alpha[ord(char) - ord("a")] += 1
 
-    @commands.command()
-    async def Ask(self, ctx, ans):
-        pass
+        word = []
+        for _ in range(len(player.answer)):
+            word.append(0)
+        for i in range(len(player.answer)):
+            if player.answer[i] == response[i]:
+                word[i] = 1  # 答案對，位置對
+                player.alpha[ord(player.answer[i]) - ord("a")] -= 1
+        for i in range(len(player.answer)):
+            if word[i] != 1:
+                if player.alpha[ord(response[i]) - ord("a")] > 0:
+                    word[i] = 2  # 答案對，位置不對
+                else:
+                    player.alpha[i] = 0  # 答案不對，位置不對
+        reply = []
+        for i in range(len(player.answer)):
+            if word[i] == 1:
+                reply.append(f":regional_indicator_{response[i]}:")
+                player.right_words += 1
+            if word[i] == 2:
+                reply.append(":twisted_rightwards_arrows:")
+            if word[i] == 0:
+                reply.append(":x:")
+        await ctx.channel.send("".join(reply))
+        if player.right_words == 5:
+            await ctx.channel.send("You win! You guessed it all!")  # 五個字母全對
+            player.in_game = False
+            return
+        for i in range(len(word)):
+            word[i] = 0
+        player.answer_times += 1
 
-        """
-        ans 是使用者傳入的猜測答案
+        if player.answer_times == 6:  # 六次未全對
+            await ctx.channel.send(f"You lose! The word is {player.answer}.")
+            player.in_game = False
+        player.right_words = 0
 
-        TODO
-        1. 沒有play直接ask : 請先輸入 Play 指令
-        2. 不是5個字的單字 : 請輸入5個字母的單字
-        3. 不是單字的英文組合(不在單字庫中) : 這好像不是個單字
-        4. 答對 : 恭喜答對!!!
-        5. 猜太多次了 : 真可惜, 答案是{answer}
-        """
+    @commands.command(pass_context=True)
+    async def play_wordle(self, ctx):
+        # global alpha, answer_times, right_words, player, answer, in_game
+        player = Player(random.choice(self.words))
+        player.in_game = True
+        # print(player.answer)
+        self.players[ctx.author] = player
+        # self.answer_times = 0
+        # self.right_words = 0
+        # self.alpha = [0 for i in range(26)]
+        # self.player = ctx.author
+        # self.answer = random.choice(self.words)
+        # self.in_game = True
+        await ctx.send(f"Game Start!")
 
 
 async def setup(bot):
