d08c395f467111818804273b71eed5df79e0084b
飛天柴犬 <54river0522@gmail.com>
2023-06-18 20:49:40 +0800

    合併TodoList功能與註解程式
    將高妮妮的程式碼合併進主線。
    為程式碼逐行註解

--- cmds/todolist.py
+++ cmds/todolist.py
@@ -2,45 +2,59 @@ import discord
 from discord.ext import commands
 import json
 from core import Cog_Extension
+from collections import defaultdict
 
 
 class TodoList(Cog_Extension):
-    # Initialization
     def __init__(self, bot):
-        self.todo = []
+        self.User_TodoList = defaultdict(list)
 
-        """
-        todo 是一個 list 變數
-        你可以在各個function中對self.todo做操作
-        來當作模擬todolist
-
-        你可能需要用到的function 
-        list : append, remove, sort
-        ctx.send(str)
-
-        """
-
-    # Add todolist
-    # item 是要增加的待辨事項
     @commands.command()
-    async def AddTodoList(self, ctx, item):
-        pass
+    async def add(self, ctx, *, item):  # add item in to list
+        todo_list = self.User_TodoList[ctx.author]  # 取出使用者TodoList
+        if not item:  # TodoList沒有內容
+            await ctx.send("請提供要添加到待辦事項清單的內容。")  # 傳送失敗訊息
+            return  # 中斷程式
+        todo_list.append(item)  # 加入TodoList
+        await ctx.send(f"已成功添加到待辦事項清單：{item}")  # 傳送成功訊息
 
-    # Remove todolist
-    # item 是要移除的待辨事項
     @commands.command()
-    async def RemoveTodoList(self, ctx, item):
-        pass
+    async def list(self, ctx):  # print list out
+        todo_list = self.User_TodoList[ctx.author]  # 取出使用者TodoList
+        if not todo_list:  # TodoList沒有內容
+            await ctx.send("待辦事項清單是空的。")  # 傳送失敗訊息
+            return  # 中斷程式
+        todo_str = "\n".join(
+            [f"{i+1}. {item}" for i, item in enumerate(todo_list)]
+        )  # 以換行串接TodoList的每項內容、index
+        await ctx.send(f"待辦事項清單：\n{todo_str}")  # 傳送TodoList內容
 
-    # Sort todolist
-    @commands.command()
-    async def SortTodoList(self, ctx):
-        pass
+    @commands.command()  # Remove index from todolist
+    async def remove(self, ctx, index: int):
+        todo_list = self.User_TodoList[ctx.author]  # 取出使用者TodoList
+        if index < 1 or index > len(todo_list):  # index小於0或超出TodoList內容數量
+            await ctx.send("請提供有效的待辦事項索引以刪除。")  # 傳送失敗訊息
+            return  # 中斷程式
+        removed_item = todo_list.pop(index - 1)  # 將內容移除
+        await ctx.send(f"已成功刪除待辦事項：{removed_item}")  # 傳送成功訊息
 
-    # Clear todolist
     @commands.command()
-    async def ClearTodoList(self, ctx):
-        pass
+    async def sort(self, ctx):  # Sort todolist
+        todo_list = self.User_TodoList[ctx.author]  # 取出使用者TodoList
+        if not todo_list:  # TodoList沒有內容
+            await ctx.send("待辦事項清單是空的。")  # 傳送失敗訊息
+            return  # 中斷程式
+        todo_list.sort()  # 排序TodoList
+        sorted_list = "\n".join(
+            [f"{i+1}. {item}" for i, item in enumerate(todo_list)]
+        )  # 以換行串接TodoList的每項內容、index
+        await ctx.send(f"按照字典序排序後的待辦事項清單：\n{sorted_list}")  # 傳送TodoList內容
+
+    @commands.command()
+    async def clear(self, ctx):  # Clear todolist
+        todo_list = self.User_TodoList[ctx.author]  # 取出使用者TodoList
+        todo_list.clear()  # 清空TodoList
+        await ctx.send("所有待辦事項已清空。")  # 傳送成功訊息
 
 
 async def setup(bot):
